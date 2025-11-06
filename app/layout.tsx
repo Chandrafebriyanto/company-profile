7a3d899a0f0d450c1f7994f049c4d750115df77d
027candrafebriyanto <febriyantocandra05@gmail.com>
2025-11-06 19:37:57 +0700

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+import Navbar from "@/components/Navbar";
+import 'remixicon/fonts/remixicon.css'
+const fontPoppins = Poppins({
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Company Profile",
@@ -27,10 +33,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${fontPoppins.className} antialiased`}>
+        <Navbar/>
+        <div className="container mx-auto px-4">{children}</div>
       </body>
     </html>
   );
---
+++ components/Navbar.jsx
@@ -0,0 +1,45 @@
+'use client'
+
+// import { log } from "console"
+import Link from "next/link"
+import {useState} from "react"
+
+function Navbar() {
+    const [active, setActive] = useState(false);
+    // console.log(active);
+
+    const handleClick = () => {
+        setActive(!active);
+    }
+
+  return (
+    <div className="navbar py-6">
+        <div className="container mx-auto px-4">
+            <div className="navbar-box flex items-center justify-between">
+                <div className="logo" >
+                    <h1 className="text-3xl font-bold">Ngoding</h1>
+                </div>
+                <ul className={`menu flex items-center gap-12 md:static absolute ${active ? "top-24 opacity-100" : "top-20 opacity-0"} left-1/2 -translate-x-1/2 md:-translate-x-0 md:flex-row flex-col md:bg-transparent bg-slate-700 w-full md:w-auto md:py-0 py-10 text-white md:text-black transition-all duration-500 md:opacity-100 md:transition-none`}>
+                    <li>
+                        <Link href={"/#home"}>Home</Link>
+                    </li>
+                    <li>
+                        <Link href={"/services"}>Services</Link>
+                    </li>
+                    <li>
+                        <Link href={"/project"}>Project</Link>
+                    </li>
+                    <li>
+                        <Link href={"/contact"}>Contact</Link>
+                    </li>
+                </ul>
+                <div className="md:hidden block" onClick={() => handleClick()}>
+                    <i className="ri-menu-line ri-2x font-bold"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
